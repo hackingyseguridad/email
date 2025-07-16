abb3165cd9bc38a4073cf8f2dc4dd899e35c6191
hackingyseguridad <antonio_taboada@hotmail.es>
2025-07-16 23:53:15 +0200

    Create send.py

---
+++ send.py
@@ -0,0 +1,35 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+try:
+    # Configuración del servidor SMTP local
+    smtp_server = "localhost"
+    smtp_port = 25
+
+    # Datos del remitente y destinatario
+    remite = "hackingyseguridad@hackingyseguridad.com"
+    destinatario = "antonio.taboada@gmail.com"
+
+    # Crear el mensaje
+    mensaje = MIMEMultipart()
+    mensaje["From"] = remite
+    mensaje["To"] = destinatario
+    mensaje["Subject"] = "prueba de envio de correo electronico"
+
+    # Contenido del mensaje
+    cuerpo = "email falso !!! http://www.hackingyseguridad.com "
+    mensaje.attach(MIMEText(cuerpo, "plain"))
+
+    # Conexión al servidor SMTP local
+    servidor_smtp = smtplib.SMTP(smtp_server, smtp_port)
+
+    # Envío del correo electrónico
+    servidor_smtp.sendmail(remite, destinatario, mensaje.as_string())
+
+    # Cerrar conexión
+    servidor_smtp.quit()
+
+    print("Send")
+except Exception as e:
+    print(e)
