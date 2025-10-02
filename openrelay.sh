3df000f44c0698145dc826bfdf01d8244fe187de
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-02 08:44:54 +0200

    Update openrelay.sh

--- openrelay.sh
+++ openrelay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo
 echo "Detecta servidores de correo salientes, abiertos, sin restringir usuario, ni IP"
-nmap -Pn $1 $2 $3 -p 25,587,465,2525,25025 --script smtp-open-relay
+nmap -Pn $1 $2 $3 -sVC -p 25,587,465,2525,25025 --script smtp-open-relay 
