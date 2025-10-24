0d3450aac323e65beaec34a61444294b9ab324ad
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-24 17:41:03 +0200

    Add comments to escaner.sh for clarity
    Added comments to describe the script's purpose.

--- escaner.sh
+++ escaner.sh
@@ -1,4 +1,8 @@
-
+#################################################
+# simple script en Bash Shell, paara escanear
+# con nmap servidores de correo electronico
+# http://www.hackingyseguridad.com/
+#################################################
 
 
 nmap  -Pn -iL ip.txt --open -n --randomize-hosts --max-retries 1   -p 25,587,465,110,143,995,993 --script "pop3-capabilities or pop3-ntlm-info" -oG resultado2.txt 
