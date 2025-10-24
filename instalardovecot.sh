16869da61ecb00b9281d578466762b15b1a0708d
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-24 17:45:06 +0200

    Update instalardovecot.sh

--- instalardovecot.sh
+++ instalardovecot.sh
@@ -1,6 +1,9 @@
-#  hackingyseguridad 2025
-
+# ###########################################
 # Instalar pop3 en Kali Linux con dovecot
+# htttp://www.hackingyseguridad.com
+############################################
+
+#!/bin/sh
 
 apt-get install dovecot-pop3d -y
 sudo systemctl restart dovecot
