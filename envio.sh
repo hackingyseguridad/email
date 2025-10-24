2f381301c508ff355ae33f96308ed83af557cc29
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-24 17:39:50 +0200

    Use variable for email address in envio.sh

--- envio.sh
+++ envio.sh
@@ -1,3 +1,7 @@
+#########################################
+# simpla prueba desde localhost de
 # envio desde consola Linux Debian
+# http://www.hackingyseguridad.com/
+########################################
 
-echo "This is the body of the email" | mail -s "This is the subject line" your_email_address
+echo "This is the body of the email" | mail -s "This is the subject line" $your_email_address
