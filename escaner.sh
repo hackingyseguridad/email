7db36e91557bad26f4bf712b56c2f6fe88c5c8d2
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-24 17:43:21 +0200

    Update escaner.sh

--- escaner.sh
+++ escaner.sh
@@ -4,7 +4,7 @@
 # http://www.hackingyseguridad.com/
 #################################################
 
-
+#!/bin/sh
 nmap  -Pn -iL ip.txt --open -n --randomize-hosts --max-retries 1   -p 25,587,465,110,143,995,993 --script "pop3-capabilities or pop3-ntlm-info" -oG resultado2.txt 
 
 
