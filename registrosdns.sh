710a3abaed33ad5ada4307a53fd35b7389ce826a
hackingyseguridad <antonio_taboada@hotmail.es>
2025-11-05 19:55:02 +0100

    Create registrosdns.sh

---
+++ registrosdns.sh
@@ -0,0 +1,18 @@
+###############################################
+# Extrae datos de las verificaciones en dominio
+# ver registros MX y TXT tipos: SPF, DKIN, DMARK
+# (r) hackingyseguridad.com  2025
+###############################################
+
+echo
+echo "Muestra los registros MX, SPF, DKIN, DMARK"
+echo "Uso ./registrosdns.sh dominio.com"
+echo
+
+dig +short MX $1
+
+dig +short TXT $1 | grep "v=spf1"
+
+dig +short TXT $1 | grep  "dkim"
+
+dig +short TXT $1 | grep  "dmarc"
