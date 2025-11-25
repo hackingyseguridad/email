c75a309dd3f7d79d6fb6f3e341149607f4607ca4
hackingyseguridad <antonio_taboada@hotmail.es>
2025-11-25 10:37:02 +0100

    Update enviolocalhost7.sh

--- enviolocalhost7.sh
+++ enviolocalhost7.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-
+postsuper -d ALL
 # requiere smarks
-# hackingyseguridad.com
+# (r) hackingyseguridad.com 2025
+# @antonio_taboada
 
 send_email_swaks() {
     local from_email="notificaciones@movistar.es"
     local to_email="antonio.taboada@telefonica.net"
 
     echo "..."
-
     swaks --to "$to_email" \
           --from "$from_email" \
           --server localhost \
           --h-Subject "Prueba" \
-          --h-From "Notificaciones Movistar <$from_email>" \
+          --h-From "Notificacion <$from_email>" \
           --h-DKIM-Signature "v=1; a=rsa-sha256; c=relaxed/relaxed; d=movistar.es; s=selector1; t=$(date +%s)" \
           --h-Authentication-Results "mx.telefonica.es; spf=pass; dkim=pass; dmarc=pass" \
-          --body "Este es un mensaje de prueba con todas las verificaciones de seguridad pasadas." \
+          --body "correo electronico de prueba con todas las verificaciones de seguridad pasadas. ;) http://www.hackingyseguridad.com/" \
           --add-header "X-Priority: 1" \
           --add-header "Importance: high"
 }
-
     send_email_swaks
-
+echo
+postqueue -f
+echo
+echo "-> Para limpiar la cola de envio del servidor SMTP en localhost: postsuper -d ALL"
+echo
+sleep 3
 mailq
+echo "..."
+echo
+
