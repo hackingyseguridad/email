25370d29e92996a35de44e17e0a91177304d369c
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-18 16:21:58 +0100

    Update openrelay.sh

--- openrelay.sh
+++ openrelay.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-echo
+#!/bin/sh
+# 
+# http://www.hackingyseguridad.com 2025
+# Antonio Taboada - simple comando para escanear y detectar servicdores de correo abiertos, sin requerir autenticación, open relay
+#
+echo "
 echo "Detecta servidores de correo salientes, abiertos, sin restringir usuario, ni IP"
+echo "
 nmap -Pn $1 $2 $3 -sVC -p 25,587,465,2525,25025 --script smtp-open-relay 
