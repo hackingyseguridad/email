97f0df8cb00f62a1db770dce7cbb6da734c7d548
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-18 16:49:22 +0100

    Simplify nmap command in escaner.sh
    Removed unnecessary parameters and added echo statement.

--- escaner.sh
+++ escaner.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 #################################################
 # simple script en Bash Shell, paara escanear
 # con nmap servidores de correo electronico
 # http://www.hackingyseguridad.com/
 #################################################
 
-#!/bin/sh
-nmap  -Pn -iL ip.txt --open -n --randomize-hosts --max-retries 1   -p 25,587,465,110,143,995,993 --script "pop3-capabilities or pop3-ntlm-info" -oG resultado2.txt 
+echo "..."
+nmap  -Pn -iL ip.txt --open -p 25,587,465,110,143,995,993 --script "pop3-capabilities or pop3-ntlm-info" -oG resultado2.txt 
 
 
