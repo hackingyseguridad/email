7eb886f3ef35c1adb00c4fae46702e9b73931c5c
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-18 17:00:42 +0100

    Update enviolocalhost5.sh

--- enviolocalhost5.sh
+++ enviolocalhost5.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Prueba de concepto
+# Prueba de concepto POC
 # suplanta a google[.]com 
 # Añade cabeceras X-Mailer modificadas
-# Añade verificaciones SPF, DJIN, DMARK
+# Añade fasifica verificaciones SPF, DJIN, DMARK
 # (R) hackingyseguridad.com 2025
 # @antonio_taboada
 
