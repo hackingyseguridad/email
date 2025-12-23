d6af37b57bd2b7aedf6d0355808f2e97590e12d5
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-23 13:23:00 +0100

    Update listanegra.sh

--- listanegra.sh
+++ listanegra.sh
@@ -1,9 +1,19 @@
-
 #!/bin/sh
-# Script simple para verificar IP puública en spamhaus
 
-# nuesta IP publica!
+# Script simple para verificar IP publica listada  en Spamhaus"
+# Verifica si una IP esta en Lista Negra, por SAPM de email
+# Las IPv4 suelen ser listadas tras el primer envio SPAM
+# Las IPv6 raramente son las litadas en BlackList
+# (R) hackingyseguridad.com 2026
+# @antonio_taboada
+
+echo "Script simple para verificar IP publica listada en Spamhaus "
+fecha_hora=$(LC_TIME=es_ES.UTF-8 date "+%d de %B de %Y %H:%M")
+echo "$fecha_hora"
+echo "Verifica si una IP esta en BlackList por SPAM email"
 IP=$(curl -s --max-time 5 ifconfig.me 2>/dev/null)
+echo "IPv4 o IPv6 actual publica !!!"
+echo $IP
 echo "Verificando IP: $IP"
 echo "------------------------"
 # resolucion IP inversa para consultas DNS
@@ -17,7 +27,7 @@ for LIST in zen.spamhaus.org sbl.spamhaus.org xbl.spamhaus.org pbl.spamhaus.org;
         echo "✓ OK en $LIST"
     fi
 done
-
 echo "------------------------"
 echo "https://check.spamhaus.org/query/ip/$IP"
+
 
