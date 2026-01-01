dd70e45a85b063af194f4aaac2e1283672ae5dfd
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-01 11:14:22 +0100

    Update enviocongmail1.sh

--- enviocongmail1.sh
+++ enviocongmail1.sh
@@ -3,7 +3,7 @@
 # Prueba de concepto POC, envio bash shell gmail
 # Script en Bash Shell 1.0 usando curl con heredo
 # Suplanta email, Spoff en el Form a google.com
-# El envelope sender (MAIL FROM en SMTP gmail) siempre será la cuenta real
+# El envelope sender (MAIL FROM en SMTP gmail) siempre será la cuenta real, aparecera la cuenta real de origen,  impresa, por el SMTP de gmail
 # (R) hackingyseguridad.com 2026
 # @antonio_taboada
 ################################################
