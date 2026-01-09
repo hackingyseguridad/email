af950f09f458d17a51704087a93f470a03f7e041
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-09 18:03:20 +0100

    Update enviocongmail.sh

--- enviocongmail.sh
+++ enviocongmail.sh
@@ -4,15 +4,18 @@
 # (R) hackingyseguridad.com 2026
 # antonio_taboada
 #####################################################
-#!/bin/sh
 real_sender="antonio.taboada@gmail.com"
 gapp_password="satk lnxi lpti vav"
 to_email="antonio.taboada@telefonica.net"
+display_name="notificaciones@google.com <notificaciones@google.com>"
+
 subject="Asunto del email"
-body="Texo del correo "
+body="Texto del correo"
+
 swaks \
     --to "$to_email" \
     --from "$real_sender" \
+    --h-From: "$display_name <$real_sender>" \
     --server smtp.gmail.com:587 \
     --auth LOGIN \
     --auth-user "$real_sender" \
@@ -20,3 +23,4 @@ swaks \
     --tls \
     --h-Subject: "$subject" \
     --body "$body"
+
