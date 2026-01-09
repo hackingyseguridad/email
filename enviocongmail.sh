dbc1ba27558be64874419eef21fe86386de257ca
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-09 13:13:26 +0100

    Update enviocongmail.sh

--- enviocongmail.sh
+++ enviocongmail.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 ##################################################
-# Prueba de concepto POC, envio bash shell gmail
+# Prueba simple de envio en bash shell con gmail
 # (R) hackingyseguridad.com 2026
-# @antonio_taboada
+# antonio_taboada
 ################################################
 
 sender="antonio.taboada@gmail.com"
 receiver="antonio.taboada@telefonica.net"
-gapp="satk lnxi lpti vavw"
+gapp="satk lnxi lpti vavv"
 sub="prueba de envio"
+body="hola!!, prueba de envio .. mas info en https://github.com/hackingyseguridad/email"
 
-body="hola!!, prueba de envio .. mas info en https://github.com/hackingyseguridad/email   "
-
-# Usa un archivo temporal para el contenido del correo
-tempfile=$(mktemp)
-echo -e "From: $sender\nTo: $receiver\nSubject: $sub\n\n$body" > "$tempfile"
-
-# Enviando correo usando curl
-response=$(curl -v --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
+curl -v --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
     --mail-from "$sender" \
     --mail-rcpt "$receiver" \
     --user "$sender:$gapp" \
-    -T "$tempfile")
+    --mail-rcpt-allowfails \
+    -T- <<EOF
+From: $sender
+To: $receiver
+Subject: $sub
+
+$body
+EOF
 
 if [ $? -eq 0 ]; then
     echo "enviado ...!"
 else
     echo "error ...!"
-    echo "Respuesta: $response"
 fi
+
