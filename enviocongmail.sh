82474d5d239d9338e1bc57431418a952afba7ccb
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-09 18:16:29 +0100

    Update enviocongmail.sh

--- enviocongmail.sh
+++ enviocongmail.sh
@@ -4,18 +4,24 @@
 # (R) hackingyseguridad.com 2026
 # antonio_taboada
 #####################################################
+
 real_sender="antonio.taboada@gmail.com"
 gapp_password="satk lnxi lpti vav"
 to_email="antonio.taboada@telefonica.net"
+
+# Aqui ponemos el nombre a mostar, para la cuenta de correo
 display_name="notificaciones@google.com <notificaciones@google.com>"
+
+# Aqui ponemos la direccion falsa que queremos que aparezca como origen
+spoff_email="notificaciones@ggoogle.com"
 
 subject="Asunto del email"
 body="Texto del correo"
 
 swaks \
     --to "$to_email" \
-    --from "$real_sender" \
-    --h-From: "$display_name <$real_sender>" \
+        --from "$real_sender" \
+        --h-From: "$display_name $spoff_email" \
     --server smtp.gmail.com:587 \
     --auth LOGIN \
     --auth-user "$real_sender" \
@@ -23,4 +29,5 @@ swaks \
     --tls \
     --h-Subject: "$subject" \
     --body "$body"
+
 
