9fff0f0132f6cfcdcd1a2d5891aa0bc5340ee9b4
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-26 09:31:37 +0100

    Update fakedns.py

--- fakedns.py
+++ fakedns.py
@@ -1,6 +1,8 @@
 # DNS falso, haremos que resuelva en local, los registros en el Script
 # Lo que no resuelve en local, lo busca e la IP del resolver en internet 
 # configuraremos en /etc/resolv.conf solo la IP 127.0.0.1
+# http://hackingyseguridad.com
+# @antonio_taboada 
 
 #!/usr/bin/env python3
 from dnslib import *
