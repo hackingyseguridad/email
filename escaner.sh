f7d182e812f3ed450386ef242ba3772c279f1ffd
hackingyseguridad <antonio_taboada@hotmail.es>
2026-06-03 08:23:42 +0200

    Update escaner.sh

--- escaner.sh
+++ escaner.sh
@@ -6,6 +6,6 @@
 #################################################
 
 echo "... "
-nmap  -Pn -iL ip.txt --open -p 25,587,465,110,143,995,993 -sVC  --script "pop3-capabilities or pop3-ntlm-info" -oG resultado2.txt -oX resultado.xml
+nmap  -Pn -iL ip.txt --open -p 25,587,465,110,143,995,993 -sVC  --script "pop3-capabilities or pop3-ntlm-info" -oG resultado.txt -oG resultado.csv -oX resultado.xml
 
 
