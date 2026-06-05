8f2c2f4f50f82f0f0e66cbf5183801f20c731688
hackingyseguridad <antonio_taboada@hotmail.es>
2026-06-05 20:17:10 +0200

    Update escaner.sh

--- escaner.sh
+++ escaner.sh
@@ -2,10 +2,10 @@
 #################################################
 # simple script en Bash Shell, paara escanear
 # con nmap servidores de correo electronico
-# http://www.hackingyseguridad.com/
+# 2026 http://www.hackingyseguridad.com/
 #################################################
 
 echo "... "
-nmap  -Pn -iL ip.txt --open -p 25,587,465,110,143,995,993 -sVC  --script "pop3-capabilities or pop3-ntlm-info" -oG resultado.txt -oG resultado.csv -oX resultado.xml
+nmap  -Pn -iL ip.txt --open -p 25,587,465,110,143,995,993 -O -sVC --script "pop3*,smtp*,imap*" -oG resultado.txt -oG resultado.csv -oX resultado.xml
 
 
